5dad533357f728b0e7628b74eb2e85cf32de625b
plastikSun <marcom2k01@gmail.com>
2023-08-25 17:47:19 +0200

    edit: removed "current" variable from if statement in changeImg, handled at end of function

--- js/main.js
+++ js/main.js
@@ -8,17 +8,15 @@ let currentImg = 0
 let lastImg = images.length - 1
 
 function changeImg() {
-    let current
+    
 
     if (this === left) {
        currentImg = (currentImg === 0) ? lastImg : currentImg - 1
-       current = images[currentImg]
-        
     } else if (this === right) {
         currentImg = (currentImg == lastImg) ? 0 : currentImg + 1
-        current = images[currentImg]
     }
     
+    let current = images[currentImg]
     imgMain.setAttribute("src","./"+current.image)
     imgTitle.innerHTML = current.title
     imgDesc.innerHTML = current.text
